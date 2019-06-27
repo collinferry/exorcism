aa6714100d5ea05b8cbff51b1c52ec0bcf9fb2db
Collin <collin.ferry@gmail.com>
2019-06-27 13:36:26 -0700

    refactor leapyear

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,13 +1,3 @@
 export const isLeap = (year) => {
-  if (year % 400 == 0) {
-    return true;
-  }
-  if (year % 100 == 0) {
-    return false;
-  }
-  if (year % 4 == 0) {
-    return true;
-  } else {
-    return false;
-  }
+  return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
 };
