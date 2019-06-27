f38f60985aa56199704a2f9f452b131109a2b11d
Collin <collin.ferry@gmail.com>
2019-06-27 11:54:24 -0700

    refactor protein

--- javascript/protein-translation/protein-translation.js
+++ javascript/protein-translation/protein-translation.js
@@ -1,8 +1,12 @@
-export function translate(input){
+export function translate(input) {
   //if empty, return blank array
-  if (typeof input === 'undefined'){ return []; }
+  if (typeof input === 'undefined') {
+    return [];
+  }
 
-  //Map codon/protein relationships (and STOP codons)
+  //define variables to hold codons, proteins, and their relationships
+  var codons = (input.match(/.{1,3}/g)); //Break input by dividing every three chars
+  var proteins = [];
   var sequenceMap = {
     "AUG": "Methionine",
     "UUU": "Phenylalanine",
@@ -23,27 +27,21 @@ export function translate(input){
     "UGA": "STOP",
   }
 
-  //Break input into codon array (dividing every three chars)
-  var codons = (input.match(/.{1,3}/g));
-
-  //Loop through array and build protein array (until you reach STOP codon)
-  var proteins = [];
-
-  for(var i=0; i<codons.length; i++){
+  //Build protein array (until reaching STOP or invalid codon)
+  for (var i = 0; i < codons.length; i++) {
     var sequence = codons[i];
 
-    // console.log(sequenceMap[sequence]);
-    if (!sequenceMap[sequence]){ throw new Error("Invalid codon"); }
+    if (!sequenceMap[sequence]) {
+      throw new Error("Invalid codon");
+    }
 
-    if(sequenceMap[sequence] == "STOP"){ break; }
-    else {
+    if (sequenceMap[sequence] == "STOP") {
+      break;
+    } else {
       proteins.push(sequenceMap[sequence]);
     }
   }
 
   return proteins;
 
-  // throw new Error("Remove this statement and implement this function");
 };
-
-// translate('LOL');
