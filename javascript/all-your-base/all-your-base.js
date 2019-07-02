307cebeb3d3b61070a67f1a50c7fd1ac3f7a8d4a
Collin <collin.ferry@gmail.com>
2019-07-02 13:45:09 -0700

    refactor your base

--- javascript/all-your-base/all-your-base.js
+++ javascript/all-your-base/all-your-base.js
@@ -1,21 +1,14 @@
 export const convert = (inputNum, startBase, endBase) => {
-
+  var decimal = 0,
+    outputNum = [];
   //error handling
-  const formatError = new Error('Input has wrong format'),
-    inputError = new Error('Wrong input base'),
-    outputError = new Error('Wrong output base');
+  const formatError = new Error('Input has wrong format');
 
-  if (Number.isInteger(startBase) == false) {
-    throw inputError;
+  if (!Number.isInteger(startBase) || (startBase <= 1)) {
+    throw new Error('Wrong input base');
   }
-  if (Number.isInteger(endBase) == false) {
-    throw outputError;
-  }
-  if (startBase <= 1) {
-    throw inputError;
-  }
-  if (endBase <= 1) {
-    throw outputError;
+  if (!Number.isInteger(endBase) || (endBase <= 1)) {
+    throw new Error('Wrong output base');
   }
   if (inputNum == "") {
     throw formatError;
@@ -27,10 +20,6 @@ export const convert = (inputNum, startBase, endBase) => {
     throw formatError;
   };
 
-
-  var decimal = 0,
-    outputNum = [];
-
   //convert input to decimal
   for (var i = 1; i <= inputNum.length; i++) {
     if ((inputNum[i] < 0) || ((inputNum[i] > 1) && (startBase == 2))) {
@@ -39,7 +28,7 @@ export const convert = (inputNum, startBase, endBase) => {
     decimal += inputNum[i - 1] * Math.pow(startBase, (inputNum.length - i));
   };
 
-  //convert decimal number to endbase
+  //convert decimal to endbase
   do {
     outputNum.unshift(decimal % endBase);
     decimal = Math.floor(decimal / endBase);
